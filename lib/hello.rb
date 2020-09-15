d409249a90f85971c205e65651e54fd753f7ce9e
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-15 01:27:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,14 @@
-def yielding_with_arguments(num)
-  puts "the program is executing the code inside the method"
-  yield(num)
-  puts "now we are back in the method"
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
