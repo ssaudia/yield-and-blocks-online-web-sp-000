9dac8a4a52d2c12cde3f0f301c54e791454d0f59
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-15 01:27:03 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,5 @@
-yielding do
-  puts "the method has yielded to the block!"
+def yielding_with_arguments(num)
+  puts "the program is executing the code inside the method"
+  yield(num)
+  puts "now we are back in the method"
 end
